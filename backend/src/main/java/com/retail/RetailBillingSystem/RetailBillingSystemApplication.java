42656e65f34d83dd9dbf39da6df170cfe6cc9415
vishsalunke14 <vishsalunke14@gmail.com>
2026-02-17 17:07:02 +0530

    springboot project created and also postgresql connected

---
+++ backend/src/main/java/com/retail/RetailBillingSystem/RetailBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.retail.RetailBillingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailBillingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/retail/RetailBillingSystem/RetailBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.RetailBillingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
